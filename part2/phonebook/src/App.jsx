280b18d9954915c61da31fc86d0eda314c48786c
Zeix <z3ixx@gmail.com>
2026-03-04 16:24:28 +0200

    Phonebook exercises

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
 import phonebookServices from './services/phonebook'
+import './App.css' 
+
+const Notification = ({message}) => <div className='notification'>{message}</div>
+
+const Error = ({errorMsg}) => <div className='error'>{errorMsg}</div>
 
 const PersonField = (props) => <div>{props.person.name} {props.person.number}<button onClick ={props.removeEntry}>delete</button></div>
 
@@ -36,36 +41,46 @@ const App = () => {
   const [newName, setNewName]= useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterName, setFilterName] = useState('');
+  const [notification, setNotification] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(()=>{
     phonebookServices
       .getAll()
       .then(persons => {
-        console.log(persons)
-        setPersons(persons)
+        console.log(persons);
+        setPersons(persons);
       })
     
   },[])
-  console.log(persons.length)
+  console.log(persons.length);
   
   function addContactHandler(e) {
     e.preventDefault();
     console.log(e);
     let findPerson =  persons.find((person)=>person.name === newName);
     if (findPerson){
-      if (confirm(`${newName} is already added to phonebook, do you want to update number?`)){
-        phonebookServices
-          .update({name: findPerson.name, number: newNumber, id: findPerson.id})
-          .then((response)=>{
-            let newPersons = [...persons];
-            newPersons[newPersons.indexOf(findPerson)] = response;
-            setPersons(newPersons);
-            setNewName('');
-            setNewNumber('');
-          })
-      }
+      if (!confirm(`${newName} is already added to phonebook, do you want to update number?`)) return; 
+
+      phonebookServices
+        .update({name: findPerson.name, number: newNumber, id: findPerson.id})
+        .then((response)=>{
+          let newPersons = [...persons];
+          newPersons[newPersons.indexOf(findPerson)] = response;
+          setPersons(newPersons);
+          setNewName('');
+          setNewNumber('');
+          setNotification(`${findPerson.name} was been updated`);
+          setTimeout( ()=> setNotification(null), 5000);
+        })
+        .catch((e) => {
+          setErrorMsg(`${findPerson.name} was already deleted`);
+          setPersons(persons.filter((person) => person.id !== findPerson.id));
+          setTimeout( ()=> setErrorMsg(null), 5000);
+        })
       return;
     }
+    
     let newPerson = {name: newName, number: newNumber};
     console.log(newPerson);
     phonebookServices
@@ -74,7 +89,10 @@ const App = () => {
         setPersons(persons.concat(response));
         setNewName('');
         setNewNumber('');
+        setNotification(`${newPerson.name} was been added`);
+        setTimeout( ()=> setNotification(null), 5000);
       })
+
 
   }
 
@@ -83,9 +101,17 @@ const App = () => {
       phonebookServices
         .remove(person.id)
         .then(response => {
-          setPersons(persons.filter((person)=>person.id != response.id))
+          setPersons(persons.filter((person)=>person.id != response.id));
+          setNotification(`${person.name} was been deleted`);
+          setTimeout( ()=> setNotification(null), 5000);
         })
-        .catch(()=>alert(`An error occured`))
+        .catch((e) => {
+          console.log('Error ocured');
+          console.log(e);
+          setErrorMsg(`${person.name} was already deleted`);
+          setPersons(persons.filter((p) => p.id !== person.id));
+          setTimeout( ()=> setErrorMsg(null), 5000);
+        })
     }
   }
 
@@ -104,6 +130,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      {notification?<Notification message ={notification}/>:<></>}
+      {errorMsg?<Error errorMsg ={errorMsg}/>:<></>}
       <Search onFilterChange={filterChangeHandler} filter={filterName}/>
       <AddContactForm nameChangeHandler={nameChangeHandler} newName={newName} numberChangeHandler={numberChangeHandler} newNumber={newNumber} addContactHandler={addContactHandler}/>
       <h2>Numbers</h2>
